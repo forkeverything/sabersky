7e62262baef22a8988f9a513b8f33c58d942b230
forkiven <mikewu1209@gmail.com>
2016-01-29 09:41:49 +0700

    Modify: Dashboard Menu Styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,23 +4,26 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{ ucfirst(Auth::user()->role->position) }} Dashboard</div>
-
-                    <div class="panel-body">
-                        <div id="dashboard-nav">
-                            <div class="nav-item">Projects</div>
-                            <div class="nav-item">Purchase Requests</div>
-                            <div class="nav-item">Vendors</div>
-                            <div class="nav-item">Items</div>
-                            <div class="nav-item">Purchase Orders</div>
-                            @can('report_view')
-                            <div class="nav-item">Reports</div>
-                            @endcan
-                            @can('settings_view')
-                            <div class="nav-item">Settings</div>
-                            @endcan
+                <h2 class="page-title">{{ ucfirst(Auth::user()->role->position) }} Dashboard</h2>
+                <div id="dashboard-nav">
+                    <div class="row">
+                        <div class="col-sm-4"><a class="dashboard-link" href="/projects">Projects</a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/purchase_requests">Purchase Requests</a>
                         </div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/vendors">Vendors</a></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4"><a class="dashboard-link" href="/items">Items</a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/purchase_orders">Purchase Orders</a>
+                        </div>
+                        @can('report_view')
+                        <div class="col-sm-4"><a class="dashboard-link" href="/reports">Reports</a></div>
+                        @endcan
+                    </div>
+                    <div class="row">
+                        @can('settings_view')
+                        <div class="col-sm-4"><a class="dashboard-link" href="/settings">Settings</a></div>
+                        @endcan
                     </div>
                 </div>
             </div>
