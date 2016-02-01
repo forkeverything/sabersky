cd9b86899e8228011bbba590ffea9f7ead47a501
forkiven <mikewu1209@gmail.com>
2016-02-01 16:04:02 +0700

    Add: Model Factory (User, Project, Items, PR)

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,46 @@
 |
 */
 
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'password' => bcrypt('password'),
         'remember_token' => str_random(10),
+        'company_id' => 1,
+        'role_id' => $faker->numberBetween(1,6)
+    ];
+});
+
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word . ' ' . $faker->numberBetween(0, 200) . ' MW',
+        'location' => $faker->city,
+        'description' => $faker->paragraph(5),
+        'company_id' => 1
+    ];
+});
+
+$factory->define(App\Item::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'specification' => $faker->paragraph(2),
+        'price_mean' => $faker->randomFloat(2,0),
+        'project_id' => factory(App\Project::class)->create()->id
+    ];
+});
+
+$factory->define(App\PurchaseRequest::class, function (Faker\Generator $faker) {
+    return [
+        'quantity' => $faker->randomDigitNotNull,
+        'due' => $faker->dateTimeThisYear,
+        'urgent' => $faker->boolean(20),
+        'state' => $faker->randomElement(['open', 'fulfilled', 'cancelled']),
+        'item_id' => factory(App\Item::class)->create()->id,
+        'project_id' => factory(App\Project::class)->create()->id,
+        'user_id' => factory(App\User::class)->create([
+            'role_id' => 2
+        ])->id
     ];
 });
