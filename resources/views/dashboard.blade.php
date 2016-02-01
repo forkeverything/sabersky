5aef7d58979bd85ad7fec3994bdbe0b6c5e1fca0
forkiven <mikewu1209@gmail.com>
2016-02-01 23:24:33 +0700

    Modify: Re-styled Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,22 +9,42 @@
                 </div>
                 <div id="dashboard-nav">
                     <div class="row">
-                        <div class="col-sm-4"><a class="dashboard-link" href="/projects">Projects & Teams</a></div>
-                        <div class="col-sm-4"><a class="dashboard-link" href="/purchase_requests">Purchase Requests</a>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/projects">
+                                <i class="fa fa-flash icon-dashboard"></i>Projects & Teams
+                            </a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/purchase_requests">
+                                <i class="fa fa-shopping-basket icon-dashboard"></i>
+                                Purchase Requests
+                            </a>
                         </div>
-                        <div class="col-sm-4"><a class="dashboard-link" href="/vendors">Vendors</a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/vendors">
+                                <i class="fa fa-truck icon-dashboard"></i>
+                                Vendors
+                            </a></div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-4"><a class="dashboard-link" href="/items">Items</a></div>
-                        <div class="col-sm-4"><a class="dashboard-link" href="/purchase_orders">Purchase Orders</a>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/items">
+                                <i class="fa fa-legal icon-dashboard"></i>
+                                Items
+                            </a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/purchase_orders">
+                                <i class="fa fa-clipboard icon-dashboard"></i>
+                                Purchase Orders
+                            </a>
                         </div>
                         @can('report_view')
-                        <div class="col-sm-4"><a class="dashboard-link" href="/reports">Reports</a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/reports">
+                                <i class="fa fa-bar-chart icon-dashboard"></i>
+                                Reports
+                            </a></div>
                         @endcan
                     </div>
                     <div class="row">
                         @can('settings_view')
-                        <div class="col-sm-4"><a class="dashboard-link" href="/settings">Settings</a></div>
+                        <div class="col-sm-4"><a class="dashboard-link" href="/settings">
+                                <i class="fa fa-gears icon-dashboard"></i>
+                                Settings
+                            </a></div>
                         @endcan
                     </div>
                 </div>
