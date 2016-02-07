5ac06c2140abd41b9d6b5609ea23f9789fabadab
forkiven <mikewu1209@gmail.com>
2016-02-07 17:05:38 +0700

    Modify: Settings format

--- resources/views/settings/show.blade.php
+++ resources/views/settings/show.blade.php
@@ -9,36 +9,44 @@
         </div>
         <p>Change Application settings to determine what needs approval for whom. Defaults have been automatically set for you.</p>
         @include('errors.list')
-        <form id="form-settings">
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="field-po-high-max">
-                    <strong>High PO Threshold</strong>
-                    <br>
-                    Purchase orders with totals over this amount will require <em>Director's</em> approval
-                </label>
-                <input type="text" id="field-po-high-max" name="po_high_max" v-model="settings.po_high_max | numberModel" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="field-po-med-max">
-                    <strong>Medium PO Threshold</strong>
-                    <br>
-                    Purchase orders with totals over this amount will require <em>Manager's</em> approval
-                </label>
-                <input type="text" id="field-po-med-max" name="po_med_max" v-model="settings.po_med_max | numberModel" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="field-item-md-max">
-                    <strong>Maximum Item Mean Difference</strong>
-                    <br>
-                    Items with a mean difference percentage over this amount will require <em>Manager's</em> approval
-                </label>
-                <input type="number" step="1" id="field-item-md-max" name="item_md_max" v-model="settings.item_md_max | percentage" class="form-control">
-            </div>
-            <!-- Submit -->
-            <div class="form-group">
-                <button type="button" class="btn btn-solid-blue form-control" @click="saveSettings">@{{ saveButtonText }}</button>
-            </div>
-        </form>
+            <form id="form-settings">
+                <div class="form-group">
+                    <label for="field-po-high-max">
+                        <strong>High PO Threshold</strong>
+                        <br>
+                        Purchase orders with totals over this amount will require <em>Director's</em> approval
+                    </label>
+                    <div class="input-group col-xs-2">
+                        <input type="text" id="field-po-high-max" name="po_high_max" v-model="settings.po_high_max | numberModel" class="form-control text-right">
+                        <span class="input-group-addon">Rp</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="field-po-med-max">
+                        <strong>Medium PO Threshold</strong>
+                        <br>
+                        Purchase orders with totals over this amount will require <em>Manager's</em> approval
+                    </label>
+                    <div class="input-group col-xs-2">
+                        <input type="text" id="field-po-med-max" name="po_med_max" v-model="settings.po_med_max | numberModel" class="form-control text-right">
+                        <span class="input-group-addon">Rp</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="field-item-md-max">
+                        <strong>Maximum Item Mean Difference</strong>
+                        <br>
+                        Items with a mean difference percentage over this amount will require <em>Manager's</em> approval
+                    </label>
+                    <div class="input-group col-xs-2">
+                        <input type="number" step="1" id="field-item-md-max" name="item_md_max" v-model="settings.item_md_max | percentage" class="form-control text-right">
+                        <span class="input-group-addon">%</span>
+                    </div>
+                </div>
+                <!-- Submit -->
+                <div class="form-group">
+                    <button type="button" class="btn btn-solid-blue form-control" @click="saveSettings">@{{ saveButtonText }}</button>
+                </div>
+            </form>
     </div>
 @endsection
